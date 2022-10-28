8a5415d786721340bae64b3072903eba76b16cd8
Sean Yang <seanyang0506@gmail.com>
2022-10-28 13:08:05 -0700

    add put

--- src/main.py
+++ src/main.py
@@ -20,6 +20,18 @@ def init_database():
     metadata['edfs']['root'] = ""
 
     metadata_json_file = json.dumps(metadata, indent=4)
+    response = requests.put(METADATA_NODE_URL, metadata_json_file)
+
+    for i in range(0, NUM_NODES):
+        node = "node" + str(i + 1)
+        node_address = get_node_address(node)
+        node_data = {}
+        node_data_json_file = json.dumps(node_data, indent=4)
+        response = requests.put(node_address, node_data_json_file)
+
+
+def get_node_address(node_name):
+    return FIREBASE_URL + node_name + ".json"
 
 
 # returns as follows:
@@ -27,9 +39,8 @@ def init_database():
 #  "block2": "second part of file",
 #  "block3": "third part of file"}
 def partition_file(file, num_pieces):
-    # f = open(file)
-    # content = f.read()
-    content = "aabbccddeeffgg"
+    f = open(file)
+    content = f.read()
     num_characters = len(content)
     piece_length = num_characters // num_pieces
     output = {}
@@ -50,18 +61,19 @@ def put_firebase_file(file, firebase_path, num_pieces):
     pass
 
 
-def get_partition_node_number(file_partition_num, file_name):
+def assign_block_to_node(file_partition_num, file_name):
     # get hash of file_name, mod by number of nodes, and return the result
-    pass
+    return hash(file_name) * (file_partition_num + 1) % NUM_NODES
 
 
 def split_path(path):
     split = path.split('/')
+    while "" in split:
+        split.remove("")
     return split
 
 
 def check_file_exists(path):
-    metadata = requests.get(METADATA_NODE_URL).text
     split = split_path(path)
 
     metadata = json.loads(requests.get(METADATA_NODE_URL).text)
@@ -69,11 +81,10 @@ def check_file_exists(path):
     current_dir = root
 
     for name in split:
-        if name != "":
-            if name in current_dir.keys():
-                current_dir = current_dir[name]
-            else:
-                return False
+        if type(current_dir) is dict and name in current_dir.keys():
+            current_dir = current_dir[name]
+        else:
+            return False
     return True
 
 
@@ -96,13 +107,61 @@ def update_meta_data(file, path, num_partitions):
     #         "node1",
     #         "node3"
     #     ] }
-    pass
+    split = split_path(path)
+    filename = split[len(split) - 1]
+    metadata = json.loads(requests.get(METADATA_NODE_URL).text)
+    root = metadata['edfs']['root']
+    if type(root) is not dict:
+        metadata['edfs']['root'] = {}
+        root = metadata['edfs']['root']
+    current_dir = root
+    for name in split:
+        if name in current_dir.keys():
+            current_dir = current_dir[name]
+        if name not in current_dir.keys():
+            current_dir[name] = {}
+            current_dir = current_dir[name]
+    current_dir['blocks'] = []
+    current_dir['block_locations'] = {}
+    for i in range(0, num_partitions):
+        block_name = "block" + str(i + 1)
+        current_dir['blocks'].append(block_name)
+        block_location1 = assign_block_to_node(i, filename)
+        block_location2 = (assign_block_to_node(i, filename) + 1) % NUM_NODES
+        current_dir['block_locations'][block_name] = []
+        current_dir['block_locations'][block_name].append("node" + str(block_location1 + 1))
+        current_dir['block_locations'][block_name].append("node" + str(block_location2 + 1))
+
+    metadata_json_file = json.dumps(metadata, indent=4)
+    response = requests.put(METADATA_NODE_URL, metadata_json_file)
+    return filename, current_dir['block_locations']
 
 
-def write_to_block(file_partitions, block_locations):
-    pass
+def get_id(filename, block):
+    return "id_" + filename + "_" + block
 
 
+def get_node_data(node_address):
+    return json.loads(requests.get(node_address).text)
+
+
+def write_to_node(node_address, nodedata):
+    metadata_json_file = json.dumps(nodedata, indent=4)
+    response = requests.put(node_address, metadata_json_file)
+
+def write_to_block(filename, file_partitions, block_locations):
+    # file_partitions: {'block1': 'abc', 'block2': 'def', 'block3': 'ghi\n'}
+    # block_locations: {'block1': ['node2', 'node0'], 'block2': ['node1', 'node2'], 'block3': ['node0', 'node1']}
+    for block in file_partitions.keys():
+        for node in block_locations[block]:
+            node_address = get_node_address(node)
+            block_id = get_id(filename, block)
+            nodedata = get_node_data(node_address)
+            print(nodedata)
+            if type(nodedata) is not dict:
+                nodedata = {}
+            nodedata[block_id] = file_partitions[block]
+            write_to_node(node_address, nodedata)
 
 
 
@@ -132,13 +191,12 @@ def rm(path):
 
 
 def put(file, path, num_partitions):
-    print("put " + file + " " + path + " " + num_partitions)
     if check_file_exists(path):
         print("File already exists!")
     else:
         file_partitions = partition_file(file, num_partitions)
-        block_locations = update_meta_data(file, path, num_partitions)
-        write_to_block(file_partitions, block_locations)
+        filename, block_locations = update_meta_data(file, path, num_partitions)
+        write_to_block(filename, file_partitions, block_locations)
 
 
 def get_partition_locations(path):
@@ -203,7 +261,7 @@ def main():
         if sys.argv[1] == "put":
             file = sys.argv[2]
             path = sys.argv[3]
-            num_partitions = sys.argv[4]
+            num_partitions = int(sys.argv[4])
             put(file, path, num_partitions)
         else:
             usage()
