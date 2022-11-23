d7b072e8750caaed204ad7b20b89821707b3eb17
Sean Yang <seanyang0506@gmail.com>
2022-11-22 22:48:01 -0800

    add test files and script

---
+++ src/test.sh
@@ -0,0 +1,7 @@
+pwd
+python3 main.py init
+python3 main.py mkdir /user
+python3 main.py mkdir /user/sean
+python3 main.py put ~/SnookerDataManager/test2.txt /user/sean/ 3
+python3 main.py rm /user/sean/test2.txt
+python3 main.py cat /user/sean/test2.txt
